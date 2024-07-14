ed2d2864fa2783d0b7583103d17f846b07d36947
MOSKANOR <mostapha.boudad.02@gmail.com>
2024-07-14 20:27:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+pip install -r req.txt
 
 # Make the script executable
-chmod +x sniffer
+chmod +x moskanor_code_alpha_sniffer.py
 
 # Copy the script to a directory in the user's PATH
-cp sniffer /usr/local/bin
+cp moskanor_code_alpha_sniffer.py /usr/local/bin
 
 echo "Installation complete."
